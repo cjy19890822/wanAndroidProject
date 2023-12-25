cdddc8b0cd3772d02daafe4503320aba1b418e97
chenjing0822 <563482721@qq.com>
2023-12-25 18:31:13 +0800

    初始化GetX

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/tabs/bindings/tabs_binding.dart';
+import '../modules/tabs/views/tabs_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+
+    GetPage(
+      name: _Paths.TABS,
+      page: () => const TabsView(),
+      binding: TabsBinding(),
+    ),
+  ];
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,16 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const TAB = _Paths.TAB;
+  static const TABS = _Paths.TABS;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const TAB = '/tab';
+  static const TABS = '/tabs';
+}
